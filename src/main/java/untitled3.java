7f08438af094dde06aa9dcf5843f8fde2355e480
ivanov7as <ivanov7as@gmail.com>
2022-05-23 11:34:50 +0300

    3

--- src/main/java/untitled3.java
+++ src/main/java/untitled3.java
@@ -66,5 +66,20 @@ public class untitled3 {
         }
     }
 
-    
+    public static void maxMin(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
 }
